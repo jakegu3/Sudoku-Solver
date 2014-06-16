9d92fdfc8a9341ecb562b1a8e84fc9b7d271efe3
dougyates <doug_yates@live.com>
2014-06-16 15:38:08 -0600

    Updated the check square function

--- Sudoku_Project_2300/Sudoku.cs
+++ Sudoku_Project_2300/Sudoku.cs
@@ -69,8 +69,29 @@ namespace Sudoku_Project_2300
 
         // The CheckSquare method
         // Purpose: Check that the 9x9 square is valid
-        public bool CheckSquare(int sr, int sc)
+        public bool CheckSquare(int row, int col)
         {
+            int r = 0, c = 0;
+
+            //count how many numbers are in the square
+            for (int i = '0'; i < '9'; i++)
+                checkAgainst[i] = 0;
+
+            for (r = row; r < row + 3; r++)
+            {
+                for (c = col; c < col + 3; c++)
+                {
+                    //check for existing numbers in the square
+                    if (board[r].ElementAt(c) != '*')
+                    {
+                        //return false if there is a duplicate number
+                        if ((checkAgainst[board[r].ElementAt(c)]) >= 1)
+                            return false;
+                        //add that the number exists in the square
+                        checkAgainst[board[r].ElementAt(c)] = 1;
+                    }
+                }
+            }
             return false;
         }
 
